912bb04d0ed8d0124f7ebcd96bdafbed263bfa4c
Ko Bkei <101390721+Bkei12@users.noreply.github.com>
2024-01-13 23:20:14 +0900

    블로그 crud 기능 추가

--- src/main/java/com/example/blog/controller/BlogApiController.java
+++ src/main/java/com/example/blog/controller/BlogApiController.java
@@ -2,14 +2,15 @@ package com.example.blog.controller;
 
 import com.example.blog.domain.Article;
 import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.dto.ArticleResponse;
+import com.example.blog.dto.UpdateArticleRequest;
 import com.example.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Resoonse Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -17,7 +18,29 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
-    // 요청 본문 값 매핑
+    // 블로그 글 전체 조회
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    // 특정 블로그 글 조회
+    @GetMapping("/api/articles/{id}")
+    //url 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    // 블로그 글 작성
     @PostMapping("/api/articles")
     // 클라이언트에서 전송된 JSON 형식의 데이터를 AddArticleRequest 객체로 변환하여 받음
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
@@ -27,6 +50,24 @@ public class BlogApiController {
         // CREATED == 201: 요청이 성공적으로 수행되었고, 새로운 리로스 생성
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    // 블로그 글 삭제
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    // 블로그 글 수정
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updateArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok().body(updateArticle);
     }
 
 }
--- src/main/java/com/example/blog/service/BlogService.java
+++ src/main/java/com/example/blog/service/BlogService.java
@@ -2,9 +2,17 @@ package com.example.blog.service;
 
 import com.example.blog.domain.Article;
 import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.dto.ArticleResponse;
+import com.example.blog.dto.UpdateArticleRequest;
 import com.example.blog.repository.BlogRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.jdbc.BlobImplementer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service
@@ -15,5 +23,36 @@ public class BlogService {
     //블로그 title, content 추가 후 Repo에 저장
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+
+
+    // 특정 블로그 글 조회
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                // 엔티티를 조회하고 id가 없으면 IllegalArgumentException 예외를 발생 시킴
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    // 모든 블로그 글 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    // 블로그 글 수정
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id ));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+
+
+    // 블로그 글 삭제
+    public void delete(long id) {
+        blogRepository.deleteById(id);
     }
 }
